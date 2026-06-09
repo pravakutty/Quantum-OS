1aa86e675e8bfc138e81362e88c0a7be5f13b1f7
pravakutty <sapraveen81900@gmail.com>
2026-06-09 09:56:01 +0530

    Fix deploy settings

--- web.py
+++ web.py
@@ -97,7 +97,7 @@ def teleportation():
     return json.dumps({'counts': counts})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000)
                                                                 
 
 
